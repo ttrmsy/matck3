a140ac50bbb5d3485a8b345265a922f6f7b7a437
ttry <138739136+ttrmsy@users.noreply.github.com>
2023-08-16 15:02:42 +0900

    stage.cpp追加
    プログラム追加

--- matck3/Stage.cpp
+++ matck3/Stage.cpp
@@ -236,14 +236,155 @@ void CreateBlock(void)
 				{
 					Block[i][j].flg = FALSE;
 					Block[i][j].x = (j - 1) * BLOCKSIZE;
-					Block[i][j].y = (j - 1) * BLOCKSIZE;
-					Block[i][j].image = NULL;
-					Block[i][j].flg = FALSE;
-					Block[i][j].image = NULL;
+					Block[i][j].y = (i - 1) * BLOCKSIZE;
+					Block[i][j].width = BLOCKSIZE;
+					Block[i][j].height = BLOCKSIZE;
+					Block[i][j].image = GetRand(7) + 1; //1~8の乱数
 				}
 			}
 		}
+
+		/*for (i = 1 < HEIGHT - 1; i++)
+		{
+			for (j = 1 < WIDTH - 1; j++)
+			{
+				if (Block[i][j].image == NULL)
+				{
+					Block[i][j].image = GetRand(7) + 1;
+				}
+			}
+		}*/
+
+		//ブロック連鎖チェック
+		for (i = 1; i < HEIGHT - 1; i++)
+		{
+			for (j = 1; j < WIDTH - 1; j++)
+			{
+				Check += combo_check(i, j);
+			}
+		}
+	} while (Check != 0);
+
+
+	for (i = 0; i < ITEM_MAX; i++)
+	{
+		Item[i] = 0;
 	}
 }
+
+
+/***********************
+*ステージ制御機能：ブロック選択処理
+* 引　数：なし
+* 戻り値：なし
+************************/
+
+void SelectBlock(void)
+{
+	int TmpBlock;
+	int Result;
+
+
+	//カーソル座標の取得
+	Select[SELECT_CURSOR].x = GetMousePositionX() / BLOCKSIZE;
+	Select[SELECT_CURSOR].y = GetMousePositionY() / BLOCKSIZE;
+
+
+	//選択ブロックの範囲を制御
+	if (Select[SELECT_CURSOR].x < 0)
+	{
+		Select[SELECT_CURSOR].x = 0;
+	}
+	if (Select[SELECT_CURSOR].x > WIDTH - 3)
+	{
+		Select[SELECT_CURSOR].x = WIDTH - 3;
+	}
+	if (Select[SELECT_CURSOR].y< 0)
+	{
+		Select[SELECT_CURSOR]. y = 0;
+	}
+	if (Select[SELECT_CURSOR].y > HEIGHT - 3)
+	{
+		Select[SELECT_CURSOR].y = WIDTH - 3;
+	}
+
+
+	//クリックでブロックを選択
+	if (GetKeyFlg(MOUSE_INPUT_LEFT)) {
+
+
+		//クリック効果音
+		PlaySoundMem(ClickSE, DX_PLAYTYPE_BACK);
+
+
+		if (ClickStatus == E_NONE) {
+			Select[NEXT_CURSOR].x = Select[SELECT_CURSOR].x;
+			Select[NEXT_CURSOR].y = Select[SELECT_CURSOR].y;
+			ClickStatus = E_ONCE;
+
+		}
+		else if (ClickStatus == E_ONCE &&
+			((abs(Select[NEXT_CURSOR].x - Select[SELECT_CURSOR].x)
+				== 1 &&
+			(abs(Select[NEXT_CURSOR].y - Select[SELECT_CURSOR].y)
+				== 0)) ||
+				(abs(Select[NEXT_CURSOR].x - Select[SELECT_CURSOR].x)
+					== 0 &&
+					abs(Select[NEXT_CURSOR].y - Select[SELECT_CURSOR].y)
+						== 1)))
+		{
+
+			Select[TMP_CURSOR].x = Select[SELECT_CURSOR].x;
+			Select[TMP_CURSOR].y = Select[SELECT_CURSOR].y;
+			ClickStatus = E_SECOND;
+		}
+
+
+
+	//選択ブロックを交換する。
+	if (ClickStatus == E_SECOND)
+
+	{
+		TmpBlock = Block[Select[NEXT_CURSOR].y + 1][Select[NEXT_CURSOR].x + 1].image;
+		Block[Select[NEXT_CURSOR].y + 1][Select[NEXT_CURSOR].x + 1].image =
+			Block[Select[TMP_CURSOR].y + 1][Select[TMP_CURSOR].x + 1].image;
+		Block[Select[TMP_CURSOR].y + 1][Select[TMP_CURSOR].x + 1].image =
+			TmpBlock;
+
+		//連鎖が３つ以上か調べる。
+		Result = 0;
+		Result += combo_check(Select[NEXT_CURSOR].y + 1,
+			Select[NEXT_CURSOR].x + 1);
+		Result += combo_check(Select[TMP_CURSOR].y + 1,
+			Select[TMP_CURSOR].x + 1);
+
+
+		//連鎖が３つ未満なら選択ブロックを元に戻す
+		if (Result == 0)
+		{
+
+
+			int TmpBlock = Block[Select[NEXT_CURSOR].y +
+				1][Select[NEXT_CURSOR].x + 1].image;
+
+			Block[Select[NEXT_CURSOR].y + 1][Select[NEXT_CURSOR].x +
+				1].image = Block[Select[TMP_CURSOR].y + 1][Select[TMP_CURSOR].x + 1].image;
+			Block[Select[TMP_CURSOR].y + 1][Select[TMP_CURSOR].x +
+				1].image = TmpBlock;
+		}
+		else
+		{
+
+
+			//連鎖が３つ以上ならブロックを消しブロック移動処理へ移行する
+			Stage_State = 1;
+		}
+
+
+		//次にクリックできるようにClockFlagを0にする
+		ClickStatus = E_NONE;
+	}
+}
+
 
 
